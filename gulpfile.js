68be498f5db2deb2029bea25708d0f47d0e0ad5a
K.K <gokennsirou@yahoo.co.jp>
2020-10-31 21:45:12 +0900

    add gulp

---
+++ gulpfile.js
@@ -0,0 +1,28 @@
+const path = require('path')
+const fs = require('fs')
+const { series, src, dest } = require('gulp')
+const sass = require('gulp-sass')
+const autoprefixer = require('gulp-autoprefixer')
+const cssmin = require('gulp-cssmin')
+
+function compile() {
+  const varsPath = 'node_modules/element-ui/packages/theme-chalk/src/common/var.scss'
+  const customVarsPath = path.resolve(process.cwd(), 'src/custom-variables.scss')
+  fs.writeFileSync(varsPath, fs.readFileSync(customVarsPath), 'utf-8')
+
+  return src('node_modules/element-ui/packages/theme-chalk/src/*.scss')
+    .pipe(sass.sync())
+    .pipe(autoprefixer({
+      cascade: false
+    }))
+    .pipe(cssmin())
+    .pipe(dest('./theme'))
+}
+
+function copyfont() {
+  return src('node_modules/element-ui/packages/theme-chalk/src/fonts/**')
+    .pipe(cssmin())
+    .pipe(dest('./theme/fonts'))
+}
+
+exports.default = series(compile, copyfont)
