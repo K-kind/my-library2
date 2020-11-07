88f30448f03042f21430f768d46ce86752de9d97
K.K <gokennsirou@yahoo.co.jp>
2020-11-08 08:41:33 +0900

    add whitelist files

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,9 @@
 <template>
   <div>
     <o-button loading></o-button>
-    <i class="el-icon-edit"></i>
+    <i class="mdi-arrow-up"></i>
+    <o-dialog visible>
+      テストダイアログ
+    </o-dialog>
   </div>
 </template>
